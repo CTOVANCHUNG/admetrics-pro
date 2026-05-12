b4c9ec4c240f3d60538693f0d76a17378887b43a
CTOVANCHUNG <napomenaantego14060@gmail.com>
2026-05-12 18:12:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Khởi tạo SDK Facebook
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : '1048996152762274', // Bạn sẽ thay số App ID vào đây ở Giai đoạn 2
+        appId      : 1048996152762274, // Bạn sẽ thay số App ID vào đây ở Giai đoạn 2
         cookie     : true,
         xfbml      : true,
         version    : 'v19.0'
